0c727525fa18f5b5861074411ef4160c8aa69124
Shashank S Shetty <smile.shashank.s.shetty1996@gmail.com>
2019-02-28 15:08:26 +0530

    router setup

---
+++ router/photos.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const config = require('universal-config');
+const Unsplash = require('unsplash-js').default;
+const toJson = require('unsplash-js').toJson;
+
+// Router instance for /api/photos
+const Router = express.Router();
+
+// Creating an unsplash object
+const unsplash = new Unsplash({
+  applicationId: config.get('APPLICATION_ID'),
+  secret: config.get('SECRET'),
+  callbackUrl: config.get('CALLBACK_URL')
+});
+
+/**
+ * @route /api/photos
+ * @access public
+ * @desc api to fetch all image details
+ */
+Router.get('/', (req, res) => {
+  unsplash.photos
+    .listPhotos(1, 30)
+    .then(toJson)
+    .then(json => res.json(json));
+});
+
+module.exports = Router;
--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 global.fetch = require('node-fetch');
+const express = require('express');
 const config = require('universal-config');
 const Unsplash = require('unsplash-js').default;
 const toJson = require('unsplash-js').toJson;
-const express = require('express');
+
+// Router
+const photos = require('./router/photos');
 
 const unsplash = new Unsplash({
   applicationId: config.get('APPLICATION_ID'),
@@ -10,15 +13,14 @@ const unsplash = new Unsplash({
   callbackUrl: config.get('CALLBACK_URL')
 });
 
+// Application instance
 const app = express();
 
-app.get('/api/photos', (req, res) => {
-  unsplash.photos
-    .listPhotos(1, 30)
-    .then(toJson)
-    .then(json => res.json(json));
-});
+// Setting up routes
+app.use('/api/photos', photos);
 
+// Port assignment
 const PORT = process.env.PORT || 5000;
 
+// Server setup
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
