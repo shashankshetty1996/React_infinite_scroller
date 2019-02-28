d94d2d674cb6e6b8fe20bfa7d22affaea9f3c393
Shashank S Shetty <smile.shashank.s.shetty1996@gmail.com>
2019-02-28 14:38:35 +0530

    Added keys

--- config/server.js
+++ config/server.js
@@ -1,5 +1,9 @@
 module.exports = {
-  APPLICATION_ID: process.env.APPLICATION_ID || '',
-  SECRET: process.env.SECRET || '',
+  APPLICATION_ID:
+    process.env.APPLICATION_ID ||
+    '044cc4c0b4c02c9ab046fdb04f469fc7c90968a3f0d8bdfad55f4b5450ccbdcf',
+  SECRET:
+    process.env.SECRET ||
+    'f0ba252c571995cd860260f72bcf6668faad5c4cac12d5dc8a65e946505ab9e7',
   CALLBACK_URL: process.env.CALLBACK_URL || 'http://localhost:3000'
 };
