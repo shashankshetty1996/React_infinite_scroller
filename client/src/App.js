0e409f6d523d44dad46f517aa792bc1d76e8d751
Shashank S Shetty <smile.shashank.s.shetty1996@gmail.com>
2019-03-04 11:03:44 +0530

    Added image component

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
+import Images from './components/Images';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1>React infinite scroll</h1>
-        </header>
+        <div className="hero is-fullheight is-bold is-info">
+          <div className="hero-body">
+            <div className="container">
+              <div className="header content">
+                <h2 className="subtitle is-6">Code Challenge #16</h2>
+                <h1 className="title is-1">
+                  Infinite Scroll Unsplash Code Challenge
+                </h1>
+              </div>
+
+              <Images />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
