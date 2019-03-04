ecf7f9cea167142c110cb20aac9e469a68343235
Shashank S Shetty <smile.shashank.s.shetty1996@gmail.com>
2019-03-04 11:08:20 +0530

    Modified landing page

--- client/src/App.js
+++ client/src/App.js
@@ -10,12 +10,10 @@ class App extends Component {
           <div className="hero-body">
             <div className="container">
               <div className="header content">
-                <h2 className="subtitle is-6">Code Challenge #16</h2>
-                <h1 className="title is-1">
-                  Infinite Scroll Unsplash Code Challenge
-                </h1>
+                <h2 className="subtitle is-6">Infinite Scroll</h2>
+                <h1 className="title is-1">Unsplash image gallery</h1>
               </div>
-
+              {/* Image render component */}
               <Images />
             </div>
           </div>
